abdb251bf6a3b407dd366d94d46d3283c70ce4d8
[] <awh938@gmail.com>
2017-12-26 10:01:06 -0500

    user retrieved

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -12,4 +13,10 @@ class Kele
 
     puts @auth_token
   end
+
+  def get_me
+    response = self.class.get('/users/me', headers: {authorization: @auth_token} )
+    JSON.parse(response.body)
+  end
+# closes class   
 end
